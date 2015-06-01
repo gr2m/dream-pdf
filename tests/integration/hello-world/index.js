b99a7d53d5777b70bc1463c439e1f492f15f0ea9
Gregor Martynus <gregor@martynus.net>
2015-06-01 13:06:49 +0300

    test: initial version

---
+++ tests/integration/hello-world/index.js
@@ -0,0 +1,27 @@
+var bufferEqual = require('buffer-equal')
+var test = require('tape')
+var fs = require('fs')
+
+var PDF = require('../../../pdf')
+
+test('hello world', function (t) {
+  t.plan(1)
+
+  var pdf = new PDF({
+    dimensions: {
+      width: 800,
+      height: 600
+    },
+    createdAt: new Date(Date.UTC(2015, 4, 9))
+  })
+
+  pdf.add('Hello, PDF world!')
+  pdf.getContent(function (error, content) {
+    if (error) throw error
+
+    var bufferGenerated = new Buffer(content, 'binary')
+    var bufferFixture = fs.readFileSync(__dirname + '/fixture.pdf')
+
+    t.equal(bufferEqual(bufferGenerated, bufferFixture), true)
+  })
+})
